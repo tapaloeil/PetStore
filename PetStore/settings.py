a35536d1ad965552da800976fd7ec51ee685dd38
Sylvain Hébert <sylvain.hebert@gmail.com>
2017-05-15 18:34:54 +0200

    list

--- PetStore/settings.py
+++ PetStore/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'PetStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'PetStore',
+        'USER': 'tapaloeil',
+        'PASSWORD': 'Accenture1',
+        'HOST': 'tapaloeil.mysql.pythonanywhere-services.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
