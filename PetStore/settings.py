72b641ab0ab1865f9bab08a0c8e347d06e84e6e1
Sylvain Hébert <sylvain.hebert@gmail.com>
2017-05-15 12:18:15 +0200

    heroku changes

--- PetStore/settings.py
+++ PetStore/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [    os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'cdn/static')
 MEDIA_URL = "/media/"
 #MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'cdn/media')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
