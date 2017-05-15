bcfd8557f38493a32f5170c14ea3ec90c736118a
Sylvain Hébert <sylvain.hebert@gmail.com>
2017-05-15 22:10:58 +0200

    list

--- PetStore/settings.py
+++ PetStore/settings.py
@@ -88,17 +88,22 @@ WSGI_APPLICATION = 'PetStore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default':{
+#        'ENGINE':'django.db.backends.mysql',
+#        'NAME':'tapaloeil$PetStore',
+#        'USER':'tapaloeil',
+#        'PASSWORD':'Mars!2017',
+#        'HOST':'tapaloeil.mysql.pythonanywhere-services.com',
+#    }
+#}
 
 DATABASES = {
-    'default':{
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'tapaloeil$PetStore',
-        'USER':'tapaloeil',
-        'PASSWORD':'Mars!2017',
-        'HOST':'tapaloeil.mysql.pythonanywhere-services.com',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
