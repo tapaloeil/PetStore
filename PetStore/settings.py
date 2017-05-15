e8376dd18bef593950bd409df6d1a9003c74c5a9
Sylvain Hébert <sylvain.hebert@gmail.com>
2017-05-15 10:20:36 +0200

    heroku changes

--- PetStore/settings.py
+++ PetStore/settings.py
@@ -248,3 +248,17 @@ JET_THEMES = [
 ]
 
 JET_SIDE_MENU_COMPACT = True
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
