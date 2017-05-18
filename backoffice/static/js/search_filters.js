bbdadaec707e8155e7a68e492e86fa80221776f3
Sylvain H <sylvain.hebert@gmail.com>
2017-05-18 17:45:26 +0200

    filtres

---
+++ backoffice/static/js/search_filters.js
@@ -0,0 +1,38 @@
+function buildFilterMenu(item){
+	if(item[0]!=""){
+		$(".filter-add > li[data-filter-dest='"+ item[0] +"'][data-text='"+item[1]+"']").children("a").removeClass("amshopby-attr");
+		$(".filter-add > li[data-filter-dest='"+ item[0] +"'][data-text='"+item[1]+"']").children("a").addClass("amshopby-attr-selected");
+
+		if ( !$( "#resetFilterButton" ).length ) {
+			$(".amshopby-filters-left").append('<button id="resetFilterButton" type="button" title="Réinitialiser les filtres" class="reset-filter-button"><span>Réinitialiser les filtres</span></button>');
+			$("#resetFilterButton").click(function(event) {
+				$("#q").val("");
+				$("#SearchForm").submit();
+			});
+		}
+	}
+}
+
+function getJsonFromUrl(query) {
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    buildFilterMenu(item);
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+}
+
+$(document).ready(function() {
+	getJsonFromUrl($("#q").val());
+	$('.amsopby-flag-clickfirst > li').click(function(event) {
+		var dataset = $(this)[0].dataset;
+		if($(this).children("a").hasClass('amshopby-attr')){
+			$("#q").val($("#q").val() + dataset.filterDest + "=" + dataset.text + "&");
+		}
+		if($(this).children("a").hasClass('amshopby-attr-selected')){
+			var str = dataset.filterDest + "=" + dataset.text + "&";
+			$("#q").val($("#q").val().replace(str,""));
+		}
+		$("#SearchForm").submit();
+	});
+});
--- backoffice/views.py
+++ backoffice/views.py
@@ -4,15 +4,39 @@ from .models import Product,ProductReferences,ProductImage,ProductType,ProductSu
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import QueryDict
+from django.db.models import Q
+from functools import reduce
 
 # Create your views here.
 def p_list(request):
-    p_list = Product.objects.all().order_by("ProductName")
+    ## Récup des données ##
+    p_list = Product.objects.all()
     pt_list = ProductType.objects.all().order_by("Type")
     pst_list=ProductSubType.objects.all().order_by("SubType")
     pb_list=ProductBrand.objects.all().order_by("Name")
-    paginator = Paginator(p_list, 12)
+    #######################
 
+    ## Search filter ##
+    q=QueryDict(request.GET.get("q"))
+    if q.__contains__("q_categorie"):
+        q_categorie=q.getlist("q_categorie")
+        p_list=Product.objects.filter(reduce(lambda x, y: x | y,[Q(ProductType__Type=item) for item in q_categorie]))
+    if q.__contains__("q_subtypeproduct"):
+        q_subtypeproduct=q.getlist("q_subtypeproduct")
+        p_list=p_list.filter(reduce(lambda x, y: x | y,[Q(ProductSubType__SubType=item) for item in q_subtypeproduct]))
+    if q.__contains__("q_brand"):
+        q_brand=q.getlist("q_brand")
+        p_list=p_list.filter(reduce(lambda x, y: x | y,[Q(Brand__Name=item) for item in q_brand]))
+    ###################
+
+    ## Ajout du tri ##
+    p_list.order_by("ProductName")
+    # à développer avec tri dynamique
+    ##################
+
+    ## Gestion de la pagination ##
+    paginator = Paginator(p_list, 12)
     page = request.GET.get('page')
     try:
         products = paginator.page(page)
@@ -22,6 +46,7 @@ def p_list(request):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         products = paginator.page(paginator.num_pages)
+    ##############################
     return render(request, 'backoffice/p_list.html',{'products':products, 'ptypes':pt_list, 'psubtypes':pst_list, 'pbrands':pb_list})
 
 def p_detail(request,pk):
