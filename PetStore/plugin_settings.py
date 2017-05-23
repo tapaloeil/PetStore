53dc8d497cec373dce23d8e758df79c2cd917bb8
Sylvain H <sylvain.hebert@gmail.com>
2017-05-23 11:16:25 +0200

    email

---
+++ PetStore/plugin_settings.py
@@ -0,0 +1,127 @@
+import os
+from django.utils.translation import ugettext_lazy as _
+
+STATIC_URL = '/static/'
+# Translation
+LANGUAGES=(
+    ('fr', _('French')),
+    ('en', _('English')),
+    ('zh-hans', _('Simplified Chinese')),
+)
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'fr'
+MODELTRANSLATION_LANGUAGES = ('fr', 'en', 'zh-hans')
+MODELTRANSLATION_PREPOPULATE_LANGUAGE = 'fr'
+MODELTRANSLATION_FALLBACK_LANGUAGES = ('en', 'fr')
+
+#Moneyed
+CURRENCIES = ('USD','EUR','CNY','HKD', 'GBP','JPY')
+
+# Registration / Login
+LOGIN_REDIRECT_URL='/'
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN=True
+
+# TinyMCE
+TINYMCE_JS_URL = STATIC_URL + "tiny_mce/tiny_mce.js"
+TINYMCE_JS_ROOT = STATIC_URL + "/tiny_mce"
+TINYMCE_SPELLCHECKER=False
+TINYMCE_PLUGINS = [
+    'safari',
+    'table',
+    'advlink',
+    'advimage',
+    'iespell',
+    'inlinepopups',
+    'media',
+    'searchreplace',
+    'contextmenu',
+    'paste',
+    'wordcount'
+]
+
+TINYMCE_DEFAULT_CONFIG={
+    'theme' : "advanced",
+    'plugins' : ",".join(TINYMCE_PLUGINS), # django-cms
+    'language' : 'ru',
+    "theme_advanced_buttons1" : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect,|,spellchecker",
+    "theme_advanced_buttons2" : "cut,copy,paste,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,cleanup,code,|,forecolor,backcolor,|,insertfile,insertimage",
+    "theme_advanced_buttons3" : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr",
+    'theme_advanced_toolbar_location' : "top",
+    'theme_advanced_toolbar_align' : "left",
+    'theme_advanced_statusbar_location' : "bottom",
+    'theme_advanced_resizing' : True,
+    'table_default_cellpadding': 2,
+    'table_default_cellspacing': 2,
+    'cleanup_on_startup' : False,
+    'cleanup' : False,
+    'paste_auto_cleanup_on_paste' : False,
+    'paste_block_drop' : False,
+    'paste_remove_spans' : False,
+    'paste_strip_class_attributes' : False,
+    'paste_retain_style_properties' : "",
+    'forced_root_block' : False,
+    'force_br_newlines' : False,
+    'force_p_newlines' : False,
+    'remove_linebreaks' : False,
+    'convert_newlines_to_brs' : False,
+    'inline_styles' : False,
+    'relative_urls' : False,
+    'formats' : {
+        'alignleft' : {'selector' : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', 'classes' : 'align-left'},
+        'aligncenter' : {'selector' : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', 'classes' : 'align-center'},
+        'alignright' : {'selector' : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', 'classes' : 'align-right'},
+        'alignfull' : {'selector' : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', 'classes' : 'align-justify'},
+        'strikethrough' : {'inline' : 'del'},
+        'italic' : {'inline' : 'em'},
+        'bold' : {'inline' : 'strong'},
+        'underline' : {'inline' : 'u'}
+    },
+    'pagebreak_separator' : "",
+    # Drop lists for link/image/media/template dialogs
+    'template_external_list_url': 'lists/template_list.js',
+    'external_link_list_url': 'lists/link_list.js',
+    'external_image_list_url': 'lists/image_list.js',
+    'media_external_list_url': 'lists/media_list.js',
+    #
+    #'file_browser_callback':'tinyDjangoBrowser'
+}
+
+
+# Jet admin interface
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': '#47bac1', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    }
+]
+
+JET_SIDE_MENU_COMPACT = True
+
+# easy thumbnail / filer
+THUMBNAIL_HIGH_RESOLUTION = True
